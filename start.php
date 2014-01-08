d4ca952f977c3f73b69c7fd52d615b14c1083dde
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2014-01-08 11:29:17 +0100

    Refactor libraries

--- start.php
+++ start.php
@@ -15,25 +15,65 @@ define('HYPEDBEXPLORER_RELEASE', 1372771756);
 
 elgg_register_event_handler('init', 'system', 'hj_db_explorer_init');
 
+/**
+ * Initialize the plugin
+ */
 function hj_db_explorer_init() {
 
-	$path = elgg_get_plugins_path() . 'hypeDBExplorer/';
+	// Register actions
+	$actions_path = dirname(__FILE__) . "/actions/db_explorer/";
 
-	$libraries = array(
-		'actions',
-		'menus',
-		'assets',
-	);
+	elgg_register_action('db_explorer/entities', $actions_path . 'entities.php', 'admin');
 
-	foreach ($libraries as $lib) {
-		$libpath = "{$path}lib/{$lib}.php";
-		if (file_exists($libpath)) {
-			elgg_register_library("dbexplorer:library:$lib", $libpath);
-			elgg_load_library("dbexplorer:library:$lib");
-		}
-	}
+	elgg_register_action('db_explorer/users_entity', $actions_path . 'users_entity.php', 'admin');
+	elgg_register_action('db_explorer/objects_entity', $actions_path . 'objects_entity.php', 'admin');
+	elgg_register_action('db_explorer/groups_entity', $actions_path . 'groups_entity.php', 'admin');
+	elgg_register_action('db_explorer/sites_entity', $actions_path . 'sites_entity.php', 'admin');
 
+	elgg_register_action('db_explorer/metadata', $actions_path . 'metadata.php', 'admin');
+	elgg_register_action('db_explorer/metadata_ownership', $actions_path . 'metadata_ownership.php', 'admin');
+
+	elgg_register_action('db_explorer/annotations', $actions_path . 'annotations.php', 'admin');
+	elgg_register_action('db_explorer/annotations_ownership', $actions_path . 'annotations_ownership.php', 'admin');
+
+	elgg_register_action('db_explorer/private_settings', $actions_path . 'private_settings.php', 'admin');
+
+	elgg_register_action('db_explorer/access_collections_ownership', $actions_path . 'access_collections_ownership.php', 'admin');
+	elgg_register_action('db_explorer/access_collections_membership', $actions_path . 'access_collections_membership.php', 'admin');
+
+	elgg_register_action('db_explorer/entity_relationships', $actions_path . 'entity_relationships.php', 'admin');
+
+	
+	// Register javascripts
+	elgg_register_js('jquery.jqgrid.js', '/mod/hypeDBExplorer/vendors/jqgrid/js/jquery.jqGrid.min.js');
+	$locale = get_language();
+	elgg_register_js('jquery.jqgrid.locale.js', "/mod/hypeDBExplorer/vendors/jqgrid/js/i18n/grid.locale-$locale.js");
+
+	elgg_register_simplecache_view('js/framework/db_explorer/jqgrid');
+	elgg_register_js('dbexplorer.jqgrid.js', elgg_get_simplecache_url('js', 'framework/db_explorer/jqgrid'));
+
+	// Register stylesheets
+	elgg_register_css('jquery.ui.css', '/mod/hypeDBExplorer/vendors/jquery.ui/css/jquery-ui.custom.css');
+	elgg_register_css('jquery.jqgrid.css', '/mod/hypeDBExplorer/vendors/jqgrid/css/ui.jqgrid.css');
+
+	elgg_register_simplecache_view('css/framework/db_explorer/jqgrid');
+	elgg_register_css('dbexplorer.jqgrid.css', elgg_get_simplecache_url('css', 'framework/db_explorer/jqgrid'));
+
+	
 	if (elgg_is_admin_logged_in()) {
+
+		// Register ajax views
 		elgg_register_ajax_view('admin/developers/db_explorer');
+
+		// Add an admin menu item
+		elgg_register_menu_item('page', array(
+			'name' => 'db_explorer',
+			'href' => 'admin/developers/db_explorer',
+			'text' => elgg_echo('admin:developers:db_explorer'),
+			'context' => 'admin',
+			'section' => 'develop'
+		));
+		
 	}
+	
 }
