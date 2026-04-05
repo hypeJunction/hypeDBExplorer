e68f8df80f35a39b94743dcfa91fa0d5f77567f8
ismayil.khayredinov@holoplot.com <ismayil.khayredinov@holoplot.com>
2026-04-05 13:39:04 +0200

    migrate(3.x): automated AST transformations
    Applied by elgg-migrate automated rules.
    See elgg-migrate rules/2x-to-3x/manifest.json for details.

--- start.php
+++ start.php
@@ -10,100 +10,63 @@
  * @copyright Copyright (c) 2013-2015, Ismayil Khayredinov
  * @license http://www.gnu.org/licenses/gpl-2.0.html GNU General Public License v2
  */
-
 namespace hypeJunction\DBExplorer;
 
 $path = __DIR__;
 if (file_exists("{$path}/vendor/autoload.php")) {
-	require_once "{$path}/vendor/autoload.php";
+    require_once "{$path}/vendor/autoload.php";
 }
 require_once __DIR__ . '/lib/hooks.php';
-
-elgg_register_event_handler('init', 'system', __NAMESPACE__ . '\\init');
-
+elgg_register_event_handler('init', 'system', __NAMESPACE__ . '\init');
 /**
  * Initialize the plugin
  * @return void
  */
-function init() {
-
-	// Register actions
-	$actions_path = dirname(__FILE__) . "/actions/db_explorer/";
-
-	elgg_register_action('db_explorer/entities', $actions_path . 'entities.php', 'admin');
-
-	elgg_register_action('db_explorer/users_entity', $actions_path . 'users_entity.php', 'admin');
-	elgg_register_action('db_explorer/objects_entity', $actions_path . 'objects_entity.php', 'admin');
-	elgg_register_action('db_explorer/groups_entity', $actions_path . 'groups_entity.php', 'admin');
-	elgg_register_action('db_explorer/sites_entity', $actions_path . 'sites_entity.php', 'admin');
-
-	elgg_register_action('db_explorer/owned_entities', $actions_path . 'owned_entities.php', 'admin');
-	elgg_register_action('db_explorer/contained_entities', $actions_path . 'contained_entities.php', 'admin');
-
-	elgg_register_action('db_explorer/river_items', $actions_path . 'river_items.php', 'admin');
-
-	elgg_register_action('db_explorer/metadata', $actions_path . 'metadata.php', 'admin');
-	elgg_register_action('db_explorer/metadata_ownership', $actions_path . 'metadata_ownership.php', 'admin');
-
-	elgg_register_action('db_explorer/annotations', $actions_path . 'annotations.php', 'admin');
-	elgg_register_action('db_explorer/annotations_ownership', $actions_path . 'annotations_ownership.php', 'admin');
-
-	elgg_register_action('db_explorer/private_settings', $actions_path . 'private_settings.php', 'admin');
-
-	elgg_register_action('db_explorer/access_collections_ownership', $actions_path . 'access_collections_ownership.php', 'admin');
-	elgg_register_action('db_explorer/access_collections_membership', $actions_path . 'access_collections_membership.php', 'admin');
-
-	elgg_register_action('db_explorer/entity_relationships', $actions_path . 'entity_relationships.php', 'admin');
-
-	elgg_register_action('db_explorer/batch', $actions_path . 'batch.php');
-
-	elgg_register_action('db_explorer/user/validate', $actions_path . 'user/validate.php');
-	elgg_register_action('db_explorer/user/ban', $actions_path . 'user/ban.php');
-	elgg_register_action('db_explorer/user/unban', $actions_path . 'user/unban.php');
-	elgg_register_action('db_explorer/user/enable', $actions_path . 'user/enable.php');
-	elgg_register_action('db_explorer/user/disable', $actions_path . 'user/disable.php');
-	elgg_register_action('db_explorer/user/delete', $actions_path . 'user/delete.php');
-
-	elgg_register_action('db_explorer/content/enable', $actions_path . 'content/enable.php');
-	elgg_register_action('db_explorer/content/disable', $actions_path . 'content/disable.php');
-	elgg_register_action('db_explorer/content/delete', $actions_path . 'content/delete.php');
-
-	// Register javascripts
-	elgg_define_js('jqgrid', array(
-		'src' => '/mod/hypeDBExplorer/vendors/jqgrid/js/minified/jquery.jqGrid.min.js',
-		'deps' => array('jquery', 'jqgrid.locale'),
-	));
-
-	$locale = get_current_language();
-	elgg_define_js('jqgrid.locale', array(
-		'src' => "/mod/hypeDBExplorer/vendors/jqgrid/js/i18n/grid.locale-$locale.js",
-		'deps' => array('jquery')
-	));
-
-	// Register stylesheets
-	//elgg_register_css('jquery.jqgrid', '/mod/hypeDBExplorer/vendor/jqgrid/css/ui.jqgrid.css');
-	elgg_register_css('db_explorer.jquery-ui', '/mod/hypeDBExplorer/vendors/jquery-ui/themes/smoothness/jquery-ui.min.css');
-	elgg_register_css('db_explorer.stylesheet', elgg_get_simplecache_url('css', 'framework/db_explorer/stylesheet.css'));
-
-
-	if (elgg_is_admin_logged_in()) {
-
-		if (elgg_get_config('debug')) {
-			// Register menu items to quickly navigate to the DB explorer for the given user/entity
-			elgg_register_plugin_hook_handler('register', 'menu:user_hover', __NAMESPACE__ . '\\user_hover_menu_setup');
-			elgg_register_plugin_hook_handler('register', 'menu:entity', __NAMESPACE__ . '\\entity_menu_setup');
-		}
-
-		// Register ajax views
-		elgg_register_ajax_view('admin/developers/db_explorer');
-
-		// Add an admin menu item
-		elgg_register_menu_item('page', array(
-			'name' => 'db_explorer',
-			'href' => 'admin/developers/db_explorer',
-			'text' => elgg_echo('admin:developers:db_explorer'),
-			'context' => 'admin',
-			'section' => 'develop'
-		));
-	}
+function init()
+{
+    // Register actions
+    $actions_path = dirname(__FILE__) . "/actions/db_explorer/";
+    elgg_register_action('db_explorer/entities', $actions_path . 'entities.php', 'admin');
+    elgg_register_action('db_explorer/users_entity', $actions_path . 'users_entity.php', 'admin');
+    elgg_register_action('db_explorer/objects_entity', $actions_path . 'objects_entity.php', 'admin');
+    elgg_register_action('db_explorer/groups_entity', $actions_path . 'groups_entity.php', 'admin');
+    elgg_register_action('db_explorer/sites_entity', $actions_path . 'sites_entity.php', 'admin');
+    elgg_register_action('db_explorer/owned_entities', $actions_path . 'owned_entities.php', 'admin');
+    elgg_register_action('db_explorer/contained_entities', $actions_path . 'contained_entities.php', 'admin');
+    elgg_register_action('db_explorer/river_items', $actions_path . 'river_items.php', 'admin');
+    elgg_register_action('db_explorer/metadata', $actions_path . 'metadata.php', 'admin');
+    elgg_register_action('db_explorer/metadata_ownership', $actions_path . 'metadata_ownership.php', 'admin');
+    elgg_register_action('db_explorer/annotations', $actions_path . 'annotations.php', 'admin');
+    elgg_register_action('db_explorer/annotations_ownership', $actions_path . 'annotations_ownership.php', 'admin');
+    elgg_register_action('db_explorer/private_settings', $actions_path . 'private_settings.php', 'admin');
+    elgg_register_action('db_explorer/access_collections_ownership', $actions_path . 'access_collections_ownership.php', 'admin');
+    elgg_register_action('db_explorer/access_collections_membership', $actions_path . 'access_collections_membership.php', 'admin');
+    elgg_register_action('db_explorer/entity_relationships', $actions_path . 'entity_relationships.php', 'admin');
+    elgg_register_action('db_explorer/batch', $actions_path . 'batch.php');
+    elgg_register_action('db_explorer/user/validate', $actions_path . 'user/validate.php');
+    elgg_register_action('db_explorer/user/ban', $actions_path . 'user/ban.php');
+    elgg_register_action('db_explorer/user/unban', $actions_path . 'user/unban.php');
+    elgg_register_action('db_explorer/user/enable', $actions_path . 'user/enable.php');
+    elgg_register_action('db_explorer/user/disable', $actions_path . 'user/disable.php');
+    elgg_register_action('db_explorer/user/delete', $actions_path . 'user/delete.php');
+    elgg_register_action('db_explorer/content/enable', $actions_path . 'content/enable.php');
+    elgg_register_action('db_explorer/content/disable', $actions_path . 'content/disable.php');
+    elgg_register_action('db_explorer/content/delete', $actions_path . 'content/delete.php');
+    // Register javascripts
+    elgg_define_js('jqgrid', array('src' => '/mod/hypeDBExplorer/vendors/jqgrid/js/minified/jquery.jqGrid.min.js', 'deps' => array('jquery', 'jqgrid.locale')));
+    $locale = get_current_language();
+    elgg_define_js('jqgrid.locale', array('src' => "/mod/hypeDBExplorer/vendors/jqgrid/js/i18n/grid.locale-{$locale}.js", 'deps' => array('jquery')));
+    // Register stylesheets
+    //elgg_register_css('jquery.jqgrid', '/mod/hypeDBExplorer/vendor/jqgrid/css/ui.jqgrid.css');
+    elgg_register_css('db_explorer.jquery-ui', '/mod/hypeDBExplorer/vendors/jquery-ui/themes/smoothness/jquery-ui.min.css');
+    elgg_register_css('db_explorer.stylesheet', elgg_get_simplecache_url('css', 'framework/db_explorer/stylesheet.css'));
+    if (elgg_is_admin_logged_in()) {
+        if (elgg_get_config('debug')) {
+            // Register menu items to quickly navigate to the DB explorer for the given user/entity
+            elgg_register_plugin_hook_handler('register', 'menu:user_hover', __NAMESPACE__ . '\user_hover_menu_setup');
+            elgg_register_plugin_hook_handler('register', 'menu:entity', __NAMESPACE__ . '\entity_menu_setup');
+        }
+        // Add an admin menu item
+        elgg_register_menu_item('page', array('name' => 'db_explorer', 'href' => 'admin/developers/db_explorer', 'text' => elgg_echo('admin:developers:db_explorer'), 'context' => 'admin', 'section' => 'develop'));
+    }
 }
--- views/default/admin/developers/db_explorer.php
+++ views/default/admin/developers/db_explorer.php
@@ -8,36 +8,20 @@
  * @uses $_GET['referrer_url'] Optional. URL to sniff for an entity GUID to be explored
  */
 elgg_require_js('framework/db_explorer');
-
 //elgg_load_css('jquery.jqgrid');
 elgg_load_css('db_explorer.jquery-ui');
 elgg_load_css('db_explorer.stylesheet');
-
 $type = get_input('type', 'user');
 $guid = get_input('guid', null);
 $url = urldecode(get_input('referrer_url', ''));
-
 if (!$guid && filter_var($url, FILTER_VALIDATE_URL)) {
-
-	elgg_load_library('db_explorer');
-	$guid = hj_db_explorer_get_guid_from_url($url);
-
-	if (!$guid) {
-		register_error(elgg_echo('db_explorer:url_sniffer_no_guid', array($url)));
-	}
+    $guid = hj_db_explorer_get_guid_from_url($url);
+    if (!$guid) {
+        register_error(elgg_echo('db_explorer:url_sniffer_no_guid', array($url)));
+    }
 }
-
 if ($guid) {
-
-	echo elgg_view('framework/db_explorer/entity', array(
-		'guid' => $guid
-	));
+    echo elgg_view('framework/db_explorer/entity', array('guid' => $guid));
 } else {
-
-	echo elgg_view('framework/db_explorer/bulk', array(
-		'type' => $type
-	));
+    echo elgg_view('framework/db_explorer/bulk', array('type' => $type));
 }
-
-
-
